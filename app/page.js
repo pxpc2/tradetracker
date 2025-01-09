1a10f3e51da85f61b0795bdcab3932725fbc41bf
Pedro Daia <pedrodaia.c@gmail.com>
2025-01-08 23:37:31 -0300

    small visual changes to homepage

--- app/page.js
+++ app/page.js
@@ -46,7 +46,7 @@ export default function Home() {
           className="aspect-1108/632 w-[69.25rem] bg-linear-to-r from-[#80caff] to-[#4f46e5] opacity-20"
         />
       </div>
-      <div className="mx-auto max-w-7xl px-6 pt-10 pb-24 sm:pb-32 lg:flex lg:px-8 lg:py-40">
+      <div className="mx-auto max-w-7xl lg:max-w-max px-6 pt-10 pb-24 sm:pb-32 lg:flex lg:px-16 lg:py-40">
         <div className="mx-auto max-w-2xl shrink-0 lg:mx-0 lg:pt-8">
           <Image src={"/rb_622.png"} width={100} height={200} alt="Logo" />
           <div className="mt-24 sm:mt-32 lg:mt-16">
